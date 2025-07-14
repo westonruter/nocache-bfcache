3a50f7c6430829981aa888a99b28e33b29f64ba5
Weston Ruter <weston@ruter.net>
2025-07-13 22:13:13 -0700

    Clarify comment in bfcache-opt-in.js

--- js/bfcache-opt-in.js
+++ js/bfcache-opt-in.js
@@ -2,10 +2,11 @@
  * Opts in to bfcache from the login screen when a user elects to "Remember Me" (and when JavaScript is enabled).
  *
  * This sets a cookie which demonstrates that JavaScript is currently enabled. This cookie only needs to live until
- * the 'attach_session_information' filter in PHP runs upon successful login. Only when JavaScript is enabled will the
- * bfcache session token cookie be set, and only when this cookie is set will the `no-store` directive be removed from
- * the `Cache-Control` response header. This is important because the pages in bfcache can only be invalidated (once the
- * user logs out) when JavaScript is enabled.
+ * the 'attach_session_information' filter in PHP runs upon successful login, hence why no expiration is set, so that
+ * the cookie will be removed when the browser session ends. Only when JavaScript is enabled (and the user has checked
+ * "Remember Me") will the bfcache session token cookie be set, and only when this cookie is set will the `no-store`
+ * directive be removed from the `Cache-Control` response header. This is important because the pages in bfcache can
+ * only be invalidated (when a user logs out) when JavaScript is enabled.
  *
  * This is a JavaScript module, so the global namespace is not polluted.
  *
