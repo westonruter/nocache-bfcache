f0e19c8d09156bbe7cc41c15dd8ff4ab97af5b4f
Weston Ruter <weston@ruter.net>
2025-07-21 16:24:46 -0700

    Send Clear-Site-Data header upon wp_logout

--- includes/bfcache-invalidation.php
+++ includes/bfcache-invalidation.php
@@ -39,3 +39,16 @@ function enqueue_bfcache_invalidation_script_modules(): void {
 foreach ( array( 'wp_enqueue_scripts', 'admin_enqueue_scripts', 'customize_controls_enqueue_scripts' ) as $_action ) {
 	add_action( $_action, __NAMESPACE__ . '\enqueue_bfcache_invalidation_script_modules' );
 }
+
+/**
+ * Sends the Clear-Site-Data header with the 'cache' directive when logging out.
+ *
+ * This only works in Chrome, and it only works in a secure context (HTTPS).
+ *
+ * @since n.e.x.t
+ * @access private
+ */
+function send_clear_site_data_upon_logout(): void {
+	header( 'Clear-Site-Data: "cache"' );
+}
+add_action( 'wp_logout', __NAMESPACE__ . '\send_clear_site_data_upon_logout' ); // TODO: Would the clear_auth_cookie action be better?
